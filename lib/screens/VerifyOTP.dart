c0c3afd4bee828080f857c0c3dbb1e2ef3afb531
horla327 <54284035+horla327@users.noreply.github.com>
2022-04-22 16:34:53 +0100

    fixed the verification ui

--- lib/screens/VerifyOTP.dart
+++ lib/screens/VerifyOTP.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:iyenova/screens/LoginScreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iyenova/screens/TransactionPinSetup.dart';
-import 'package:pinput/pinput.dart';
 import 'package:iyenova/widgets/appbutton.dart';
 
 class VerificationScreen extends StatefulWidget {
@@ -16,140 +16,144 @@ class VerificationScreen extends StatefulWidget {
 }
 
 class _VerificationScreenState extends State<VerificationScreen> {
-  // final TextEditingController _pinPutController = TextEditingController();
-  // final FocusNode _pinPutFocusNode = FocusNode();
-  // final BoxDecoration pinPutDecoration = BoxDecoration(
-  //   color: const Color.fromRGBO(43, 46, 66, 1),
-  //   borderRadius: BorderRadius.circular(10.0),
-  //   border: Border.all(
-  //     color: const Color.fromRGBO(320, 203, 224, 1),
-  //   ),
-  // );
-  final defaultPinTheme = PinTheme(
-    width: 50,
-    height: 50,
-    textStyle: TextStyle(
-        fontSize: 20,
-        color: Color.fromRGBO(30, 60, 87, 1),
-        fontWeight: FontWeight.w600),
-    decoration: BoxDecoration(
-      border: Border.all(color: Colors.black),
-      borderRadius: BorderRadius.circular(5),
-    ),
-  );
-
-  // final focusedPinTheme = defaultPinTheme.copyDecorationWith(
-  //   border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
-  //   borderRadius: BorderRadius.circular(8),
-  // );
-
-  // final submittedPinTheme = defaultPinTheme.copyWith(
-  //   decoration: defaultPinTheme.decoration.copyWith(
-  //     color: Color.fromRGBO(234, 239, 243, 1),
-  //   ),
-  // );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.black,
-              ),
-              onPressed: (() {
-                Navigator.pushNamed(context, LoginScreen.id);
-              }),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'OTP Verification',
-              style: GoogleFonts.poppins(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black),
-            ),
-            SizedBox(height: 30),
-            Text(
-              'Just one more step left to go',
-              style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black),
-            ),
-            Text(
-              'Please enter the OTP sent to +234 818 648 1346',
-              style: GoogleFonts.poppins(fontSize: 12, color: Colors.black),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Pinput(
-                defaultPinTheme: defaultPinTheme,
-                // focusedPinTheme: focusedPinTheme,
-                // submittedPinTheme: submittedPinTheme,
-                validator: (s) {
-                  return s == '222222' ? null : 'Pin is incorrect';
-                },
-                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-                showCursor: true,
-                onCompleted: (pin) => print(pin),
-              ),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Didn't recieve the code?",
-                  style: GoogleFonts.poppins(fontSize: 11, color: Colors.black),
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
                 ),
-                Text(
-                  " Resend OTP",
-                  style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black),
-                ),
-                // TextButton(
-                //   child: Text(
-                //     "Sign in",
-                //     style: GoogleFonts.poppins(
-                //         fontSize: 11,
-                //         fontWeight: FontWeight.w700,
-                //         color: Colors.black),
-                //   ),
-                //   onPressed: () {
-                //     Navigator.pushNamed(context, LoginScreen.id);
-                //   },
-                // ),
-              ],
-            ),
-            SizedBox(
-              height: 270,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: AppButton(
-                label: 'Verify',
-                backgroundColor: Colors.black,
-                onPressed: () {
-                  Navigator.pushNamed(context, TransactionPinScreen.id);
-                },
+                onPressed: (() {
+                  Navigator.pushNamed(context, LoginScreen.id);
+                }),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'OTP Verification',
+                style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Just one more step left to go',
+                style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black),
+              ),
+              Text(
+                'Please enter the OTP sent to +234 818 648 1346',
+                style: GoogleFonts.poppins(fontSize: 12, color: Colors.black),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  PinTextField(),
+                  SizedBox(width: 10.0),
+                  PinTextField(),
+                  SizedBox(width: 10.0),
+                  PinTextField(),
+                  SizedBox(width: 10.0),
+                  PinTextField(),
+                  SizedBox(width: 10.0),
+                  PinTextField(),
+                  SizedBox(width: 10.0),
+                  PinTextField(),
+                ],
+              ),
+              SizedBox(height: 60.0),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Didn't recieve the code?",
+                    style:
+                        GoogleFonts.poppins(fontSize: 11, color: Colors.black),
+                  ),
+                  Text(
+                    " Resend OTP",
+                    style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black),
+                  ),
+                  // TextButton(
+                  //   child: Text(
+                  //     "Sign in",
+                  //     style: GoogleFonts.poppins(
+                  //         fontSize: 11,
+                  //         fontWeight: FontWeight.w700,
+                  //         color: Colors.black),
+                  //   ),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, LoginScreen.id);
+                  //   },
+                  // ),
+                ],
+              ),
+              SizedBox(
+                height: 300,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: AppButton(
+                  label: 'Verify',
+                  backgroundColor: Colors.black,
+                  onPressed: () {
+                    Navigator.pushNamed(context, TransactionPinScreen.id);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       )),
+    );
+  }
+}
+
+class PinTextField extends StatelessWidget {
+  const PinTextField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 45,
+      width: 45,
+      child: TextFormField(
+        onChanged: (value) {
+          if (value.length == 1) {
+            FocusScope.of(context).nextFocus();
+          }
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+        style: Theme.of(context).textTheme.headline6,
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly,
+        ],
+      ),
     );
   }
 }
